5e405e569d85135d6149d9674c6ae7f1b810ea45
andersonjf1978 <140839895+andersonjf1978@users.noreply.github.com>
2023-10-05 17:27:24 -0300

    05102023

---
+++ lib/17_asserts/1_asserts.dart
@@ -0,0 +1,12 @@
+import 'cliente.dart';
+
+void main(){
+
+  var cli = Cliente(
+    cpf: '12312321',
+    nome: 'dfjfjsbfjsd',
+    razaoSocial: '12312321',
+    cnpj: '5565456'
+    
+    );
+}
---
+++ lib/17_asserts/cliente.dart
@@ -0,0 +1,14 @@
+class Cliente {
+  String? nome;
+  String? cpf;
+
+  String? razaoSocial;
+  String? cnpj;
+
+  Cliente({
+    this.nome,
+    this.cpf,
+    this.razaoSocial,
+    this.cnpj,
+  });
+}
